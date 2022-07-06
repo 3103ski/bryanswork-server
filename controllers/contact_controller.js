0ea9649071dd4ab300e50fa31cf8dc46ae98d38e
3103ski <bjastski@gmail.com>
2022-07-06 13:41:13 -0700

    troubleshooting prod server

--- controllers/contact_controller.js
+++ controllers/contact_controller.js
@@ -15,8 +15,9 @@ exports.add_contact_submission = async function (formData) {
 
 exports.add_tms_contact_submission = async function (formData) {
 	if (!formData) return { error: { msg: 'Missing form data' } };
+	console.log({ formData });
 	const newSubmission = await new TMSContactSubmission(formData);
-
+	console.log({ newSubmission });
 	return newSubmission
 		.save()
 		.then((formSubmission) => ({ success: { data: formSubmission._doc, message: 'success', status: 200 } }))
